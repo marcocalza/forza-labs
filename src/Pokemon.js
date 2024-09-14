623f486212b24a2e2248d7863671c08759204a40
marcocalza <marcocalza.it@gmail.com>
2024-09-15 00:02:40 +0200

    Adding Flag functionality to get the code sstart from first search, testing it i get the wrong result, still getting problems with rendering the data properly

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -9,12 +9,13 @@ import { Card, Image, Text, Title, Loader, Button, Center, Container, Grid, Text
 const Pokemon = () => {
     const [pokemon, getPokemon] = useState(null);
     const [searchQuery, setQuery] = useState(''); 
+    const [searchFlag, setsearchFlag] = useState(false); // Flag to make pokemon not to be 'null' without being searched first
 
     // Change the value of query with new input event
     
     const ChangeQuery = (eventSearch) => {
         setQuery(eventSearch.target.value);
-    }
+    };
 
     // Handling search input and pass the query to the fetch
 
@@ -25,44 +26,58 @@ const Pokemon = () => {
         else {
             console.error(':/ You searched for an empty name');
         }
-    }
+    };
 
     // Fetching data to get the pokemon 
 
     const fetchPokemon = (pokemonName) => {
-        fetch('https://pokeapi.co/api/v2/pokemon/${pokemonName}')
+        fetch('https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}')
         .then((pokemonData) => pokemonData.json())
         .then((pokemonData) => {
             getPokemon(pokemonData);
             console.log(pokemonData);
+            setsearchFlag(true);
         })
-        .catch(console.error);
-        getPokemon(null);
-    }
+        .catch((error) => {
+            console.error('No pokemon found.');
+            getPokemon(null);
+            setsearchFlag(true);
+        });
+    };
 
     // Handle the variables in HTML where user can type, send and retrieve back the data
-
+    
+    return (
+        <Container>
+            <div className="pokemon-search">
+                <TextInput value={searchQuery} onChange={ChangeQuery} placeholder="Search Pokemon name..."/>
+                <Button onClick={searchPokemon}>Search</Button>
+            </div>
+        </Container>
+    );
     if (pokemon) {
-        return (
-            <Container>
-                <div className="pokemon-search">
-                    <TextInput value={searchQuery} onChange={ChangeQuery} placeholder="Search Pokemon name..."/>
-                    <Button onClick={searchPokemon}>Search</Button>
-                </div>
-                <div className="pokemon-card">
-                    <h1> {pokemon.name} </h1>
-                    <Image src={pokemon.image}/>
-                </div>
-            </Container>
-        );
+        if (searchFlag) {
+            return (
+                <Container>
+                    <div className="pokemon-card">
+                            <h1> {pokemon.name} </h1>
+                        <Image src={pokemon.image}/>
+                        <p> Weight: {pokemon.weight}</p>
+                        <p> Height: {pokemon.height}</p>
+                    </div>
+                </Container>
+            );
+        } else {
+            return (
+                <Container>
+                    <p>No Pokemon found. Please search for a Pokemon.</p>
+                </Container>
+            );
+        };
     } else {
         return (
             <Container>
-                <div className="pokemon-search">
-                    <TextInput value={searchQuery} placeholder="Search Pokemon name..."/>
-                    <Button onClick={searchPokemon}>Search</Button>
-                </div>
-                <p>No Pokemon found. Please search for a Pokemon.</p>
+                <p>No Pokemon Found.</p>
             </Container>
         );
     };
